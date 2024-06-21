1e9e9ca34b2b5052dfd5348c05380bef108275d1
bingzaza <139136066+bingzaza@users.noreply.github.com>
2024-06-21 23:47:14 +0800

    Delete bird classfication directory

--- bird classfication/models/resnet.py
+++
@@ -1,7 +0,0 @@
-import torchvision.models as models
-import torch.nn as nn
-
-def get_resnet18(num_classes=200):
-    model = models.resnet18(pretrained=True)
-    model.fc = nn.Linear(model.fc.in_features, num_classes)
-    return model
